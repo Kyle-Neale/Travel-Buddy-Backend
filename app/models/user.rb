0db7492e34755b9643672120f770e002122980f7
Kyle-Neale <kyle.a.neale@gmail.com>
2018-10-11 14:27:32 -0400

    Added a couple of models I think are necessary for the App.

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_one_attached :avatar
+  has_many :friendships
+  has_many :spots
+  has_many :friends, through: :friendships
+  has_many :places, through: :spots
+
+  has_secure_password
+end
